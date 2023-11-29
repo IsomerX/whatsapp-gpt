ec36fe06f84b6d6c588f4ac5f8414ff25188b390
dhruv bakshi <dhruvbakshi12343@gmail.com>
2023-11-30 01:40:03 +0530

    changes to life

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 
-const client = new Client();
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    handleSIGINT: false,
+  },
+});
 
+process.on("SIGINT", async () => {
+  console.log("(SIGINT) Shutting down...");
+  await client.destroy();
+  console.log("client destroyed");
+  process.exit(0);
+});
 
 client.on("qr", (qr) => {
   // Generate and scan this code with your phone
   console.log("QR RECEIVED", qr);
+  qrcode.generate(qr, { small: true });
 });
 
 client.on("ready", () => {
@@ -13,24 +26,22 @@ client.on("ready", () => {
 });
 
 client.on("message", async (msg) => {
-  console.log(msg);
-  console.log(msg.from);
   if (msg.body == "!ping") {
     msg.reply("pong");
     return;
   }
-  if (msg.hasMedia){
+  if (msg.hasMedia) {
     console.log("Media is not supported");
     // msg.reply("Media is not supported");
     return;
   }
-  if (msg.from.includes("@g")){
+  if (msg.from.includes("@g")) {
     console.log("Group Conversations are not supported");
     // msg.reply("Group Conversations are not supported");
     return;
   }
 
-  const mobile = "+" + msg.from.split('@')[0];
+  const mobile = "+" + msg.from.split("@")[0];
   const message = msg.body;
 
   if (msg.body == "!new") {
@@ -41,7 +52,6 @@ client.on("message", async (msg) => {
     msg.reply("Welcome to PhoneGPT");
     return;
   }
-
 
   const response = await fetch("http://localhost:3000/api/response", {
     method: "POST",
@@ -54,14 +64,14 @@ client.on("message", async (msg) => {
   console.log("MOBILE: ", mobile, "STATUS: ", status);
   console.log("REPLY: ", reply);
 
-  switch (status){
+  switch (status) {
     case 200:
       msg.reply(reply);
       return;
 
     case 404:
       return;
-    
+
     case 400:
       return;
 
