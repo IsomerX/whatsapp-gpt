34bde47a3bad421e28a1a9440aff2097b1a739d0
Shivam Mittal <shivammittal2124@gmail.com>
2023-11-30 00:40:12 +0530

    new conversation support

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-import { z } from "zod";
-
 const { Client } = require("whatsapp-web.js");
 
 const client = new Client();
 
-const ChatResponseRequestBody = z.object({
-  mobile: z.string().min(5).max(15),
-  message: z.string().min(1),
-});
 
 client.on("qr", (qr) => {
   // Generate and scan this code with your phone
@@ -20,41 +14,64 @@ client.on("ready", () => {
 
 client.on("message", async (msg) => {
   console.log(msg);
+  console.log(msg.from);
   if (msg.body == "!ping") {
     msg.reply("pong");
     return;
   }
 
-  const mobile = msg.from;
+  const mobile = "+" + msg.from.split('@')[0];
   const message = msg.body;
 
+  if (msg.body == "!new") {
+    await fetch("http://localhost:3000/api/new", {
+      method: "POST",
+      body: JSON.stringify({ mobile }),
+    });
+    msg.reply("Welcome to PhoneGPT");
+    return;
+  }
+
+
+  try{
   const response = await fetch("http://localhost:3000/api/response", {
     method: "POST",
     body: JSON.stringify({ mobile, message }),
   });
-
-  const status = response?.status;
-  const reply = response?.message;
-
-  if (status === 200) {
-    msg.reply(reply);
+  } catch (e){
+    if (msg.hasMedia) {
+      console.log("Media is not supported")
+      // msg.reply("Media is not supported");
+      return;
+    }
+    console.log("Group Conversations are not supported");
+    // msg.reply("Group Conversations are not supported")
     return;
   }
 
-  if (status === 404){
-    return;
-  }
+  const status = response.status;
+  const data = await response.json();
+  const reply = data?.message;
+  console.log("MOBILE: ", mobile, "STATUS: ", status);
+  console.log("REPLY: ", reply);
 
-  if (status === 400){
-    msg.reply(" You have used up all your free messages. Please get an add-on.")
-    return;
-  }
+  switch (status){
+    case 200:
+      msg.reply(reply);
+      return;
 
-  if (status === 500){
-    msg.reply("Something went wrong. Please try again later.")
-    return;
+    case 404:
+      return;
+    
+    case 400:
+      return;
+
+    case 500:
+      return;
+
+    default:
+      return;
   }
-  // SEND MESSAGE AND PHONE NUMBER TO BACKEND AND GET RESPONSE AND REPLY
 });
 
 client.initialize();
